9b5e1c5ae9d56dc4525658a006f37c2ee5418fa4
THK <thk@localexpressdaegu.org>
2026-09-15 23:12:38 +0900

    over39-2026: r36

--- over39-2026/src/v13/dashboard.js
+++ over39-2026/src/v13/dashboard.js
@@ -1,4 +1,4 @@
-import { buildConnectionProfile, connectionTopics, coordinateInsight, rankedMatches } from "./connection.js?v=v7-20260913-r35";
+import { buildConnectionProfile, connectionTopics, coordinateInsight, rankedMatches } from "./connection.js?v=v7-20260915-r36";
 
 const root = document.querySelector("#dashboard-root");
 const pendingKey = "over39-v13-pilot-pending-submission";
